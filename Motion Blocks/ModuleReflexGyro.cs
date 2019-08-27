b849b282f164da5dc2526a99ef0406fb6752df6f
Arniel <aceba1@outlook.com>
2019-08-27 02:24:30 -0400

    Create ModuleReflexGyro.cs

---
+++ Motion Blocks/ModuleReflexGyro.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+namespace MotionBlocks
+{
+    public class ModuleReflexGyro : Module
+    {
+        /// <summary>
+        /// Aid in all axis, or only Yaw
+        /// </summary>
+        public bool AllAxis;
+        public float Strength = 200;
+        public float MaxStrength = 200f;
+        private Vector3 TiltInput;
+        public Transform Drum;
+        public Vector3 Debt = Vector3.zero;
+        public float MemoryExtent = 2f;
+
+        public void OnPool()
+        {
+            block.AttachEvent.Subscribe(new Action(this.OnAttach));
+            block.DetachEvent.Subscribe(new Action(this.OnDetach));
+            Drum = transform.Find("Gyro_Drum");
+        }
+
+        private void OnAttach()
+        {
+            base.block.tank.control.driveControlEvent.Subscribe(new Action<TankControl.ControlState>(this.OnControlInput));
+        }
+
+        private void OnDetach()
+        {
+            block.tank.control.driveControlEvent.Unsubscribe(new Action<TankControl.ControlState>(this.OnControlInput));
+            TiltInput = Vector3.zero;
+            Debt = Vector3.zero;
+        }
+
+        private void OnControlInput(TankControl.ControlState controlState)
+        {
+            TiltInput = controlState.InputRotation * MaxStrength;
+        }
+
+        public void FixedUpdate()
+        {
+            if (block.tank)
+            {
+                Transform rootblock = block.tank.rootBlockTrans;
+                Rigidbody rbody = block.tank.rbody;
+
+                var localAngularVelocity = rootblock.InverseTransformVector(rbody.angularVelocity) * Strength + TiltInput;
+                if (!AllAxis)
+                {
+                    var projectionAxis = rootblock.InverseTransformDirection(transform.up);
+                    localAngularVelocity = Vector3.Project(localAngularVelocity, projectionAxis);
+                }
+                if (block.tank.beam.IsActive)
+                    Debt = Debt * 0.95f;
+
+                Debt = Vector3.ClampMagnitude(Debt + localAngularVelocity, MaxStrength * MemoryExtent);
+
+                float _m = Mathf.Min(rbody.mass + 0f, 25f) / 25f;
+                var torque = rootblock.TransformVector(Vector3.ClampMagnitude(Debt, MaxStrength)) * _m;
+                rbody.AddTorque(-torque, ForceMode.Impulse);
+                Drum.Rotate(transform.InverseTransformVector(torque) * 0.2f, Space.Self);
+            }
+        }
+    }
+}
