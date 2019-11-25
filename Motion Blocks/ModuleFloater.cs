274493972ea1b8b0670c712664ba1d9774d1c1da
Arniel <aceba1@outlook.com>
2019-11-25 08:15:56 -0500

    Allow negative MaxStrength

--- Motion Blocks/ModuleFloater.cs
+++ Motion Blocks/ModuleFloater.cs
@@ -13,10 +13,14 @@ namespace MotionBlocks
             if (block.IsAttached && block.tank != null && !block.tank.beam.IsActive)
             {
                 Vector3 blockCenter = block.centreOfMassWorld;
-                block.tank.rbody.AddForceAtPosition(Vector3.up * Mathf.Clamp(
-                        (MaxStrength / MaxHeight) * (MaxHeight - blockCenter.y) 
-                        - block.tank.rbody.GetPointVelocity(blockCenter).y * VelocityDampen, 0f, MaxStrength * 1.25f), 
-                    blockCenter, ForceMode.Impulse);
+                float blockForce = (MaxStrength / MaxHeight) * (MaxHeight - blockCenter.y)
+                      - block.tank.rbody.GetPointVelocity(blockCenter).y * VelocityDampen;
+                if (MaxStrength > 0)
+                    block.tank.rbody.AddForceAtPosition(Vector3.up * Mathf.Clamp(blockForce, 0f, MaxStrength * 1.25f), 
+                        blockCenter, ForceMode.Impulse);
+                else
+                    block.tank.rbody.AddForceAtPosition(Vector3.up * Mathf.Clamp(blockForce, MaxStrength * 1.25f, 0f),
+                        blockCenter, ForceMode.Impulse);
             }
         }
     }
